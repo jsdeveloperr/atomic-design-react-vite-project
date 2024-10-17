b95e55294aca985b76b4699a018248df1f683045
Abdulnasır Olcan <abdulnasirolcan@Abdulnasrs-MacBook-Pro.local>
2024-10-17 21:42:12 +0300

    React.FC removed

--- src/components/templates/AuthTemplate.tsx
+++ src/components/templates/AuthTemplate.tsx
@@ -4,7 +4,7 @@ import LoginForm from '@organisms/LoginForm';
 
 type AuthTemplateProps = {}; // You can define props if needed
 
-const AuthTemplate: React.FC = (props: AuthTemplateProps) => {
+const AuthTemplate = (props: AuthTemplateProps) => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="w-full max-w-md p-8 space-y-6 bg-white shadow-md rounded-lg">
